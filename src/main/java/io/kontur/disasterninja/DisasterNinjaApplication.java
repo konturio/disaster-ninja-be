d22a655dfd3db6a4efe60f44f3e265eb172d242e
Alexander Zapasnik <sanych27@gmail.com>
2021-10-01 12:39:32 +0300

    Application stub

---
+++ src/main/java/io/kontur/disasterninja/DisasterNinjaApplication.java
@@ -0,0 +1,13 @@
+package io.kontur.disasterninja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterNinjaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisasterNinjaApplication.class, args);
+    }
+
+}
