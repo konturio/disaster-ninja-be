6842682d30b5cf4368686563f4db45d0a04afdd5
Kira Stepina <kstsepina@kontur.io>
2021-11-11 16:28:51 +0300

    #7880 Add rounding

--- src/main/java/io/kontur/disasterninja/service/AnalyticsTabService.java
+++ src/main/java/io/kontur/disasterninja/service/AnalyticsTabService.java
@@ -76,7 +76,11 @@ public class AnalyticsTabService {
                             .setScale(0, RoundingMode.HALF_UP).intValue());
                 }
                 else {
-                    text.append(functionsResultsMap.get(function.getId())).append(" ").append(function.getPostfix()).append(" ");
+                    text.append(BigDecimal.valueOf(functionsResultsMap.get(function.getId()))
+                            .setScale(2, RoundingMode.HALF_UP).doubleValue())
+                            .append(" ")
+                            .append(function.getPostfix())
+                            .append(" ");
                 }
             }
             dto.setName(field.getName());
