52536c886af51d122be39ebee44ea30a24a5f199
Ilya Semichastnov <isemichastnov@kontur.io>
2022-02-15 16:04:24 +0400

    #9037 create,update,delete via /layers for user small point layers: removed redundant validation from LayerCreateDto

--- src/main/java/io/kontur/disasterninja/dto/layer/LayerCreateDto.java
+++ src/main/java/io/kontur/disasterninja/dto/layer/LayerCreateDto.java
@@ -1,16 +1,10 @@
 package io.kontur.disasterninja.dto.layer;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class LayerCreateDto extends LayerUpdateDto {
-    @NotNull
-    @NotEmpty
-    @Pattern(regexp = "[\\w]*")
     private String id;
 }
