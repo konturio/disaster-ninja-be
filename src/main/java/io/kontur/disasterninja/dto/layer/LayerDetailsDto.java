b7de49c78632cf364aeb99464826b67b306bf5b2
Alexander Zapasnik <sanych27@gmail.com>
2022-03-24 14:02:00 +0100

    US962 Hotfix for layer details legend

--- src/main/java/io/kontur/disasterninja/dto/layer/LayerDetailsDto.java
+++ src/main/java/io/kontur/disasterninja/dto/layer/LayerDetailsDto.java
@@ -1,7 +1,6 @@
 package io.kontur.disasterninja.dto.layer;
 
 import io.kontur.disasterninja.domain.Layer;
-import io.kontur.disasterninja.domain.Legend;
 import lombok.Data;
 
 @Data
@@ -11,11 +10,11 @@ public class LayerDetailsDto {
     private final Integer maxZoom; //for 'vector' and 'raster' only (see source.type)
     private final Integer minZoom; //for 'vector' and 'raster' only (see source.type)
     private final LayerSourceDto source;
-    private final Legend legend;
+    private final StyleRuleDto legend;
     private final boolean ownedByUser;
 
     public static LayerDetailsDto fromLayer(Layer layer) {
         return layer == null ? null : new LayerDetailsDto(layer.getId(), layer.getMaxZoom(), layer.getMinZoom(),
-            LayerSourceDto.fromLayerSource(layer.getSource()), layer.getLegend(), layer.isOwnedByUser());
+            LayerSourceDto.fromLayerSource(layer.getSource()), StyleRuleDto.fromLegend(layer.getLegend()), layer.isOwnedByUser());
     }
 }
