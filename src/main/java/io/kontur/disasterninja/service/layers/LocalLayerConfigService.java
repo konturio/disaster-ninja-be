2dee08487990c2b39619f087f98412077069ba7d
Alexander Zapasnik <sanych27@gmail.com>
2022-04-05 10:01:23 +0200

    #9715 MR fix

--- src/main/java/io/kontur/disasterninja/service/layers/LocalLayerConfigService.java
+++ src/main/java/io/kontur/disasterninja/service/layers/LocalLayerConfigService.java
@@ -34,7 +34,7 @@ public class LocalLayerConfigService implements LayerConfigService {
                                    @Value("${spring.profiles.active:dev}") String profile) {
         try {
             LocalLayersConfig localLayersConfig = convertLayerConfig(layersJsonResource);
-            localLayersConfig.getConfigs().forEach(this::setLegendStepsOrder);
+            localLayersConfig.getConfigs().forEach(this::setLegendStepsOrderIfRequired);
 
             localLayersConfig.getConfigs().forEach((config) -> {
                 if (!config.isTestOnly() || !"prod".equalsIgnoreCase(profile)) {
@@ -107,7 +107,7 @@ public class LocalLayerConfigService implements LayerConfigService {
         return globalOverlays;
     }
 
-    private void setLegendStepsOrder(Layer layer) {
+    private void setLegendStepsOrderIfRequired(Layer layer) {
         if (layer.getLegend() != null && layer.getLegend().getSteps() != null) {
             List<LegendStep> steps = layer.getLegend().getSteps();
             for (int i = 0; i < steps.size(); i++) {
