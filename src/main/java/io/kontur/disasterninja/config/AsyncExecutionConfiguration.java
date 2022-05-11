dba7efa416017baee6daf02f7a98972d82c3d293
Alexander Zapasnik <sanych27@gmail.com>
2022-05-11 12:27:53 +0200

    #10207 Refactor Layers providers parallel invocation

--- src/main/java/io/kontur/disasterninja/config/AsyncExecutionConfiguration.java
+++ src/main/java/io/kontur/disasterninja/config/AsyncExecutionConfiguration.java
@@ -6,9 +6,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.concurrent.DelegatingSecurityContextRunnable;
 
-import javax.annotation.PostConstruct;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -16,19 +15,16 @@ import java.util.concurrent.ThreadPoolExecutor;
 @EnableAsync(proxyTargetClass = true)
 public class AsyncExecutionConfiguration extends AsyncConfigurerSupport {
 
-    @PostConstruct
-    protected void init() {
-        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
-    }
-
     @Override
     @Primary
     @Bean
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(Runtime.getRuntime().availableProcessors() - 1);
         executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors() - 1);
         executor.setThreadNamePrefix("async-thread-");
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setTaskDecorator(DelegatingSecurityContextRunnable::new);
         executor.initialize();
 
         return executor;
