d72789c2b4588bfe2b764d5f1659ca031226938d
Pavel Repin <prepin@kontur.io>
2022-06-29 09:08:25 +0200

    #10511 Add per auth counter metric

--- src/main/java/io/kontur/disasterninja/client/InsightsApiClientImpl.java
+++ src/main/java/io/kontur/disasterninja/client/InsightsApiClientImpl.java
@@ -1,12 +1,17 @@
 package io.kontur.disasterninja.client;
 
+import com.apollographql.apollo.ApolloClient;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Counter;
+import io.prometheus.client.Summary;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
-@RequiredArgsConstructor
 @ConditionalOnProperty(name = "kontur.platform.insightsApi.url")
 public class InsightsApiClientImpl implements InsightsApiClient {
 
@@ -14,8 +19,34 @@ public class InsightsApiClientImpl implements InsightsApiClient {
 
     private final RestTemplate insightsApiRestTemplate;
 
+    private final static String AUTHENTICATED = "AUTHENTICATED";
+
+    private final static String NOT_AUTHENTICATED = "NOT_AUTHENTICATED";
+
+    private final Counter metrics;
+
+    public InsightsApiClientImpl(RestTemplate insightsApiRestTemplate, MeterRegistry meterRegistry) {
+        this.insightsApiRestTemplate = insightsApiRestTemplate;
+        Counter.Builder metricsBuilder = Counter.build()
+                .labelNames("authenticated")
+                .name("http_server_authenticated_requests")
+                .help("Requests divided by auth token.");
+
+        if (meterRegistry instanceof PrometheusMeterRegistry) {
+            CollectorRegistry collectorRegistry = ((PrometheusMeterRegistry) meterRegistry).getPrometheusRegistry();
+            metrics = metricsBuilder.register(collectorRegistry);
+        } else {
+            metrics = metricsBuilder.create();
+        }
+    }
+
     @Override
     public byte[] getBivariateTileMvt(Integer z, Integer x, Integer y, String indicatorsClass) {
+        if ("all".equals(indicatorsClass)) {
+            metrics.labels(AUTHENTICATED).inc();
+        } else {
+            metrics.labels(NOT_AUTHENTICATED).inc();
+        }
         return insightsApiRestTemplate.getForEntity(String.format(INSIGHTS_API_TILE_MVT_URI, z, x, y, indicatorsClass), byte[].class)
                 .getBody();
     }
