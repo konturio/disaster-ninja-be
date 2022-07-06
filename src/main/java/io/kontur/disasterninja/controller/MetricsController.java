c0e39272bf5de923b90fde6f176f109c989865d3
Alexander Zapasnik <sanych27@gmail.com>
2022-07-06 14:50:19 +0200

    #11041 Add endpoint for RUM metrics

--- src/main/java/io/kontur/disasterninja/controller/MetricsController.java
+++ src/main/java/io/kontur/disasterninja/controller/MetricsController.java
@@ -10,11 +10,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/rum")
 public class MetricsController {
 
     private final Summary summary;
