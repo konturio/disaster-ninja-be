fe388b923ba81e0b96725425c18217c4f10dc0ed
Pavel Repin <prepin@kontur.io>
2022-07-20 13:14:40 +0200

    Revert "#10722 add info about indicators for bivariate legend (#12)"
    This reverts commit 6b41474ee2e1cb6c0c30f629d2db3a7c64a0d160.

--- src/main/java/io/kontur/disasterninja/service/layers/providers/BivariateLayerProvider.java
+++ src/main/java/io/kontur/disasterninja/service/layers/providers/BivariateLayerProvider.java
@@ -155,17 +155,6 @@ public class BivariateLayerProvider implements LayerProvider {
             }
             xAxis.setLabel(x.label());
             xAxis.setQuotient(x.quotient());
-
-            if (x.quotients() != null) {
-                List<BivariateLegendQuotient> quotients = requireNonNull(x.quotients()).stream()
-                        .map(quotient -> BivariateLegendQuotient.builder()
-                                .name(quotient.name())
-                                .label(quotient.label())
-                                .direction(quotient.direction())
-                                .build())
-                        .toList();
-                xAxis.setQuotients(quotients);
-            }
         }
         //AXIS 2
         BivariateLegendAxisDescription yAxis = new BivariateLegendAxisDescription();
@@ -179,20 +168,8 @@ public class BivariateLayerProvider implements LayerProvider {
                                 .build()).toList();
                 yAxis.setSteps(steps);
             }
-
             yAxis.setLabel(y.label());
             yAxis.setQuotient(y.quotient());
-
-            if (y.quotients() != null) {
-                List<BivariateLegendQuotient> quotients = requireNonNull(y.quotients()).stream()
-                        .map(quotient -> BivariateLegendQuotient.builder()
-                                .name(quotient.name())
-                                .label(quotient.label())
-                                .direction(quotient.direction())
-                                .build())
-                        .toList();
-                yAxis.setQuotients(quotients);
-            }
         }
         //colors matrix
         List<ColorDto> colors = requireNonNull(overlay.colors()).stream()
@@ -210,14 +187,14 @@ public class BivariateLayerProvider implements LayerProvider {
     }
 
     private List<String> copyrightsFromIndicators(Legend legend, List<BivariateLayerLegendQuery.Indicator> indicators) {
-        Set<BivariateLegendQuotient> quotients = new HashSet<>();
-        quotients.addAll(legend.getAxes().getX().getQuotients());
-        quotients.addAll(legend.getAxes().getY().getQuotients());
+        Set<String> quotient = new HashSet<>();
+        quotient.addAll(legend.getAxes().getX().getQuotient());
+        quotient.addAll(legend.getAxes().getY().getQuotient());
 
         Set<String> copyrights = new HashSet<>();
-        quotients.forEach(q ->
+        quotient.forEach(q ->
                 indicators.stream()
-                        .filter(indicator -> Objects.equals(indicator.name(), q.getName()))
+                        .filter(indicator -> Objects.equals(indicator.name(), q))
                         .findFirst()
                         .map(BivariateLayerLegendQuery.Indicator::copyrights)
                         .orElseGet(ArrayList::new)
