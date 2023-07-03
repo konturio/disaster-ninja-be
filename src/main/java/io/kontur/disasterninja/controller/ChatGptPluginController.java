2b94a64b6df789e92218368dd902bdf49439a776
Ivan Shilyaev <ishilyaev@kontur.io>
2023-07-03 11:15:32 +0200

    #16088 Update API

--- src/main/java/io/kontur/disasterninja/controller/ChatGptPluginController.java
+++ src/main/java/io/kontur/disasterninja/controller/ChatGptPluginController.java
@@ -2,13 +2,11 @@ package io.kontur.disasterninja.controller;
 
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
 
@@ -18,23 +16,17 @@ public class ChatGptPluginController {
 
     @GetMapping("/.well-known/ai-plugin.json")
     public ResponseEntity<InputStreamResource> getPluginManifest() {
-        try (InputStream pluginManifest = getClass().getClassLoader().getResourceAsStream("ai-plugin.json")) {
-            return ResponseEntity.ok()
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .body(new InputStreamResource(Objects.requireNonNull(pluginManifest)));
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
+        InputStream pluginManifest = getClass().getClassLoader().getResourceAsStream("ai-plugin.json");
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new InputStreamResource(Objects.requireNonNull(pluginManifest)));
     }
 
     @GetMapping("/openapi.json")
     public ResponseEntity<InputStreamResource> getOpenApiSpec() {
-        try (InputStream openApiSpec = getClass().getClassLoader().getResourceAsStream("openapi.json")) {
-            return ResponseEntity.ok()
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .body(new InputStreamResource(Objects.requireNonNull(openApiSpec)));
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
+        InputStream openApiSpec = getClass().getClassLoader().getResourceAsStream("openapi.json");
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new InputStreamResource(Objects.requireNonNull(openApiSpec)));
     }
 }
