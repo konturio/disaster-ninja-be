9fbf22c6dc45bf77991e8727262d4bbab5ecf5c0
Alexander Kolesen <alexander.kolesen@gmail.com>
2024-02-21 12:39:02 +0100

    Sentry integration (#116)

--- src/main/java/io/kontur/disasterninja/DisasterNinjaApplication.java
+++ src/main/java/io/kontur/disasterninja/DisasterNinjaApplication.java
@@ -1,5 +1,6 @@
 package io.kontur.disasterninja;
 
+import io.sentry.Sentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
