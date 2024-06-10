b78042e2a3a8ab55375a621d105131450dbeb499
Polina Krukovich <polina.krukovich7@gmail.com>
2024-06-10 11:39:55 +0200

    #18745 allow access to /bivariate_matrix if user has bivariate manager or mcda feature in any app

--- src/main/java/io/kontur/disasterninja/controller/BivariateMatrixController.java
+++ src/main/java/io/kontur/disasterninja/controller/BivariateMatrixController.java
@@ -3,18 +3,22 @@ package io.kontur.disasterninja.controller;
 import io.kontur.disasterninja.dto.bivariatematrix.BivariateMatrixDto;
 import io.kontur.disasterninja.dto.bivariatematrix.BivariateMatrixRequestDto;
 import io.kontur.disasterninja.service.BivariateMatrixService;
+import io.kontur.disasterninja.service.UserProfileService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Tag(name = "Bivariate matrix", description = "Bivariate matrix API")
 @RestController
@@ -23,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BivariateMatrixController {
 
     private final BivariateMatrixService bivariateMatrixService;
+    private final UserProfileService userProfileService;
 
     @Operation(summary = "Calculate data for bivariate matrix using insights-api service",
             tags = {"Bivariate matrix"},
@@ -30,11 +35,12 @@ public class BivariateMatrixController {
     @ApiResponse(responseCode = "200", description = "Successful operation",
             content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = BivariateMatrixDto.class)))
     @PostMapping
-    @PreAuthorize("hasRole('bivariateStatistic')")
-    public BivariateMatrixDto getBivariateMatrix(
+    public ResponseEntity<BivariateMatrixDto> getBivariateMatrix(
             @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Polygon in GeoJSON format. Send polygon as FeatureCollection")
             @RequestBody(required = false) BivariateMatrixRequestDto bivariateMatrixRequestDto) {
-
-        return bivariateMatrixService.getDataForBivariateMatrix(bivariateMatrixRequestDto);
+        if (userProfileService.userHasAccessToFeature(List.of("mcda", "bivariate_color_manager", "bivariate_manager"))) {
+            return ResponseEntity.ok(bivariateMatrixService.getDataForBivariateMatrix(bivariateMatrixRequestDto));
+        }
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
 }
--- src/main/java/io/kontur/disasterninja/service/UserProfileService.java
+++ src/main/java/io/kontur/disasterninja/service/UserProfileService.java
@@ -2,12 +2,16 @@ package io.kontur.disasterninja.service;
 
 import io.kontur.disasterninja.client.UserProfileClient;
 import io.kontur.disasterninja.controller.exception.WebApplicationException;
+import io.kontur.disasterninja.dto.AppDto;
+import io.kontur.disasterninja.dto.AppSummaryDto;
 import io.kontur.disasterninja.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -41,5 +45,22 @@ public class UserProfileService {
             LOG.error("Can't update current user: {}", e.getMessage(), e);
             throw new WebApplicationException("Can't update current user", HttpStatus.BAD_GATEWAY);
         }
+    }
+
+    /**
+     * Checks if user has access to any of the provided features
+     * @param featureNames feature names to check user access
+     * @return true if user has access to at least one of the provided features,
+     *         false otherwise
+     */
+    public boolean userHasAccessToFeature(List<String> featureNames) {
+        List<AppSummaryDto> appSummaries = client.getAppsList();
+        for (AppSummaryDto appSummary : appSummaries) {
+            AppDto app = client.getApp(appSummary.getId());
+            if (app.getFeatures().stream().anyMatch(feature -> featureNames.contains(feature.getName()))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
