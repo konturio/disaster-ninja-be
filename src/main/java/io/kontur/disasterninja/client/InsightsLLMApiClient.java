e03ea73a82c7c6928db47a101e0853f1a1950add
Nadia <hh.sleeping@gmail.com>
2024-08-26 19:23:37 +0400

    create search endpoint (#155)
    * create search endpoint
    
    * Update src/main/java/io/kontur/disasterninja/client/InsightsLLMApiClient.java
    
    Co-authored-by: Alexander Chichigin <sad.ronin@gmail.com>
    
    ---------
    
    Co-authored-by: Alexander Chichigin <sad.ronin@gmail.com>

--- src/main/java/io/kontur/disasterninja/client/InsightsLLMApiClient.java
+++ src/main/java/io/kontur/disasterninja/client/InsightsLLMApiClient.java
@@ -2,15 +2,16 @@ package io.kontur.disasterninja.client;
 
 import io.kontur.disasterninja.dto.AnalyticsRequestDto;
 import io.kontur.disasterninja.dto.LLMAnalyticsDto;
+import io.kontur.disasterninja.dto.SearchDto;
 import io.kontur.disasterninja.service.KeycloakAuthorizationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 import static org.springframework.http.HttpMethod.*;
 
@@ -32,6 +33,14 @@ public class InsightsLLMApiClient extends RestClientWithBearerAuth {
                 .exchange("/llm-analytics", POST, httpEntityWithUserBearerAuthIfPresent(llmAnalyticsRequest),
                         new ParameterizedTypeReference<>() {
                         });
+        return response.getBody();
+    }
+
+    public SearchDto search(UUID appId, String query) {
+        ResponseEntity<SearchDto> response = llmAnalyticsRestTemplate
+                .exchange(String.format("/search?appId=%s&query=%s", appId, query), GET,
+                        httpEntityWithUserBearerAuthIfPresent(null),
+                        new ParameterizedTypeReference<>() {});
         return response.getBody();
     }
 }
