73912ae6cb17b4c14a6f092a85641ab0224d3722
Nadia <hh.sleeping@gmail.com>
2024-09-17 16:38:01 +0400

    add /search/mcda_suggestion endpoint (#162)
    * add /search/mcda_suggestion endpoint
    
    * preauthorize user for mcda_suggestion
    
    * fix method naming for mcda_suggestion

--- src/main/java/io/kontur/disasterninja/controller/SearchController.java
+++ src/main/java/io/kontur/disasterninja/controller/SearchController.java
@@ -51,4 +51,20 @@ public class SearchController {
             @RequestBody SearchClickRequestDto searchClickRequest) {
         return llmAnalyticsClient.logSearchClick(searchClickRequest);
     }
+
+    @Operation(summary = "mcda suggestion",
+            tags = {"Search"},
+            description = "Generate MCDA from search query using AI")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "OK"),
+        @ApiResponse(responseCode = "400", description = "Bad Request: invalid input data"),
+        @ApiResponse(responseCode = "401", description = "Unauthorized: User not authenticated"),
+    })
+    @GetMapping("/mcda_suggestion")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<byte[]> mcdaSuggestion(
+                @RequestParam(name = "appId", required = true) UUID appId,
+                @RequestParam(name = "query", required = true) String query) {
+        return llmAnalyticsClient.mcdaSuggestion(appId, query);
+    }
 }
