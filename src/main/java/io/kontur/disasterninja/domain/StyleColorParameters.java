10b4447c0898f8bfe301cf7be4e61ed7942fb9c7
Alex Chichigin <sad.ronin@gmail.com>
2024-11-21 20:09:29 +0400

    Add fill-antialias to color parameters

--- src/main/java/io/kontur/disasterninja/domain/StyleColorParameters.java
+++ src/main/java/io/kontur/disasterninja/domain/StyleColorParameters.java
@@ -19,6 +19,8 @@ public class StyleColorParameters {
     private List<List<Object>> fillColor;
     @JsonProperty("fill-opacity")
     private Double fillOpacity;
+    @JsonProperty("fill-antialias")
+    private Boolean fillAntialias;
     // sentiments
     private String bad;
     private String good;
