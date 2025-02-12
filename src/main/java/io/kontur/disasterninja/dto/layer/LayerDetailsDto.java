d62ddbd4f74cba78199edd668a4b63f7bea4ed70
Alex Chichigin <sad.ronin@gmail.com>
2025-02-12 19:46:35 +0400

    Transfer the style as a JSON object

--- src/main/java/io/kontur/disasterninja/dto/layer/LayerDetailsDto.java
+++ src/main/java/io/kontur/disasterninja/dto/layer/LayerDetailsDto.java
@@ -1,5 +1,7 @@
 package io.kontur.disasterninja.dto.layer;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import io.kontur.disasterninja.domain.Layer;
 import io.kontur.disasterninja.domain.Legend;
 import io.kontur.disasterninja.util.JsonUtil;
@@ -9,12 +11,12 @@ import io.kontur.disasterninja.util.JsonUtil;
  * @param minZoom for 'vector' and 'raster' only (see source.type)
  */
 public record LayerDetailsDto(String id, Integer maxZoom, Integer minZoom, LayerSourceDto source, Legend legend,
-                              String style, String popupConfig, boolean ownedByUser) {
+                              ObjectNode style, String popupConfig, boolean ownedByUser) {
 
     public static LayerDetailsDto fromLayer(Layer layer) {
         return layer == null ? null : new LayerDetailsDto(layer.getId(), layer.getMaxZoom(), layer.getMinZoom(),
-                LayerSourceDto.fromLayer(layer), layer.getLegend(),
-                layer.getMapStyle() != null ? JsonUtil.writeJson(layer.getMapStyle()) : null,
-                layer.getPopupConfig() != null ? JsonUtil.writeJson(layer.getPopupConfig()) : null, layer.isOwnedByUser());
+                LayerSourceDto.fromLayer(layer), layer.getLegend(), layer.getMapStyle(),
+                layer.getPopupConfig() != null ? JsonUtil.writeJson(layer.getPopupConfig()) : null, // TODO: get rid of PopupConfig, noone uses it for ages
+                layer.isOwnedByUser());
     }
 }
