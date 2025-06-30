eba70be0e302f9ef50c783652e29a34df8f99b57
Darafei Praliaskouski <me@komzpa.net>
2025-06-30 15:32:54 +0400

    Fix Slack formatting for > characters (#202)
    * Fix Slack message formatting for > characters
    
    * Update src/main/java/io/kontur/disasterninja/notifications/slack/SlackMessageFormatter.java
    
    * Update src/test/java/io/kontur/disasterninja/notifications/slack/SlackMessageFormatterTest.java
    
    * Update src/test/java/io/kontur/disasterninja/notifications/slack/SlackMessageFormatterTest.java
    
    ---------
    
    Co-authored-by: pcatchu <204916877+pcatchu@users.noreply.github.com>

---
+++ src/test/java/io/kontur/disasterninja/notifications/slack/SlackMessageFormatterTest.java
@@ -0,0 +1,13 @@
+package io.kontur.disasterninja.notifications.slack;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SlackMessageFormatterTest {
+
+    @Test
+    public void sanitizeEventNameReplacesProblemCharacters() {
+        String result = SlackMessageFormatter.sanitizeEventName("Event >= 5");
+        assertEquals("Event ≥ 5", result);
+    }
+}
