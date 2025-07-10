d1af4b4eb4a3640a2a3b00958a50d085553973f2
Nadia <hh.sleeping@gmail.com>
2025-07-10 17:25:06 +0400

    Handle 'none' as missing feed

--- src/main/java/io/kontur/disasterninja/notifications/NotificationsProcessor.java
+++ src/main/java/io/kontur/disasterninja/notifications/NotificationsProcessor.java
@@ -84,7 +84,7 @@ public class NotificationsProcessor {
     @Scheduled(fixedRate = 60000, initialDelay = 1000)
     public void run() {
         processFeed(eventApiFeed);
-        if (eventApiFeed2 != null) {
+        if (eventApiFeed2 != null && !"none".equalsIgnoreCase(eventApiFeed2)) {
             processFeed(eventApiFeed2);
         }
     }
